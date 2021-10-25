128d4d23dee526cdc41199acfbe0df33a9aed4cd
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-10-25 14:00:35 +0200

    initial setup

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { combineReducers, createStore, compose, applyMiddleware } from "redux"
+import jobReducer from "../reducers/job"
+import favoriteReducer from "../reducers/favorite"
+import thunk from "redux-thunk"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const initialState = {
+
+    favorite:{
+        companies: [],
+    },
+    job:{
+        jobs:[],
+        // isError: false,
+        // isLoading: false,
+    }
+    
+}
+
+const mainReducer = combineReducers({
+    favorite: favoriteReducer,
+    job: jobReducer,
+})
+
+
+const configureStore = createStore(
+    mainReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+)
+
+export default configureStore
